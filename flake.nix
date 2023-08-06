8c9df624e1d6b13fbf86d1519ec8be6e06ace5d9
Chloe Bethel <chloe@9net.org>
2023-08-06 11:35:25 +0100

    flatten name

--- flake.nix
+++ flake.nix
@@ -7,25 +7,25 @@
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
-  {
-    homeConfigurations.stary = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-      extraSpecialArgs = { inherit inputs; };
-      modules = [ ({ ... }: {
-        home.homeDirectory = "/home/stary";
-        home.username = "stary";
-        home.stateVersion = "22.11";
+  outputs = inputs: {
+    homeConfigurations = builtins.foldl' (x: y: x // y) {} (map (system:
+      {
+        "stary-${system}" = inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          extraSpecialArgs = { inherit inputs; };
+          modules = [ ({ ... }: {
+            home.homeDirectory = "/home/stary";
+            home.username = "stary";
+            home.stateVersion = "22.11";
 
-        programs.home-manager.enable = true;
-        programs.git.enable = true;
-        programs.htop.enable = true;
-        programs.tmux.enable = true;
-      }) ];
-    };
-  });
+            programs.home-manager.enable = true;
+            programs.git.enable = true;
+            programs.htop.enable = true;
+            programs.tmux.enable = true;
+          }) ];
+        };
+      }) ["x86_64-linux" "aarch64-linux"]);
+  };
 }
